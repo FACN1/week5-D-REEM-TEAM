0f0ca1659a68536b89c80960fb595de00e0acc0e
Macintosh Helper <noreply@macintoshhelper.com>
2017-03-21 17:58:28 +0200

    Add shot import for server tests (fix)

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -1,6 +1,7 @@
 const test = require('tape');
 const router = require('../router');
 const mockData = require('./fixtures/mockData');
+const shot = require('shot');
 
 test('Test that tape is working', (t) => {
     t.ok(true);
@@ -11,16 +12,16 @@ test('Test router with possible routes', (t) => {
     // Test router:
     // Make sure that it redirects to correct handler
 
-    Shot.inject(router, { method: 'get', url: '/' }, (res) => {
+    shot.inject(router, { method: 'get', url: '/' }, (res) => {
         t.equal(res.statusCode, 200, '/ should have statusCode 200 because the URL exists');
     });
-    Shot.inject(router, { method: 'get', url: '/search' }, (res) => {
+    shot.inject(router, { method: 'get', url: '/search' }, (res) => {
         t.equal(res.statusCode, 200, '/search should have statusCode 200 because the URL exists');
     });
-    Shot.inject(router, { method: 'get', url: '/latest' }, (res) => {
+    shot.inject(router, { method: 'get', url: '/latest' }, (res) => {
         t.equal(res.statusCode, 200, '/latest should have statusCode 200 because the URL exists');
     });
-    Shot.inject(router, { method: 'get', url: '/2934o9siudf9asndu934' }, (res) => {
+    shot.inject(router, { method: 'get', url: '/2934o9siudf9asndu934' }, (res) => {
         t.equal(res.statusCode, 404, '/2934o9siudf9asndu934 should have statusCode 404 because the URL doesn\'t exist');
     });
 });

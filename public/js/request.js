e6e57eb1238104a0e56e8ffcbd566a47256bad89
Macintosh Helper <noreply@macintoshhelper.com>
2017-03-22 12:45:51 +0200

    Write xhr makeRequest function with error first callback in public/js/request.js

--- public/js/request.js
+++ public/js/request.js
@@ -2,9 +2,26 @@
 // sends requests to server-side index.js / server.js
 
 var Request = (function() {
-    var makeRequest = () => {
-
+    var makeRequest = (callback) => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange((e) => {
+            if (xhr.readyState === 4)
+                if (xhr.statusCode === 200) {
+                    callback(null, JSON.parse(xhr.responseText));
+                } else {
+                    callback(new Error('Status code:' + xhr.statusCode));
+                }
+            }
+        })
     }
+
+    makeRequest((error, data) => {
+        if (error) {
+            console.log(error.message);
+            return;
+        }
+
+    });
 
     return {
         makeRequest: makeRequest

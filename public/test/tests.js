fe439d047aa79439b6be11fe5a77f41c218e205e
Mario <mario.saliba98@gmail.com>
2017-03-22 12:09:58 +0200

    adds function and tests for formatQuery

--- public/test/tests.js
+++ public/test/tests.js
@@ -11,10 +11,18 @@ QUnit.test( "hello test", function( assert ) {
 
 // formatQuery
 // Does it return a string?
-QUnit.test("Test if outputs a string", function(assert) {
-  assert.equal()
+QUnit.test("Testing FormatQuery Module", function(assert) {
+  assert.equal(FormatQuery.formatQuery("mario"), "/search?q=mario", "returns an actual string");
+  assert.equal(FormatQuery.formatQuery(1234).message, "Input is not a string", "Check if returns non string");
+  assert.equal(FormatQuery.formatQuery("").message, "Input can't be empty", "Check if return empty string");
+  assert.equal(FormatQuery.formatQuery().message, "Input can't be empty", "Check if returns nothing");
+  // Does it remove upper case?
+
+  assert.equal(FormatQuery.formatQuery("ReeM"), "/search?q=reem", "Check if input changes to lowercase");
+  assert.equal(FormatQuery.formatQuery("   ").message, "Input must include letters", "check if returns trimmed string and not falsey");
+  assert.equal(FormatQuery.formatQuery(" fff  "), "/search?q=%20fff%20%20", "check if returns trimmed string ");
+
 });
-// Does it remove upper case?
 // Does it replace spaces with '%20'?
 // Does it trim white space on either end
 // Does it remove special characters?

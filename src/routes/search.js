2f51ae5119b142f2a374be31aa5dbe4ad612663a
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-22 14:33:45 +0200

    routes/search.js: add search.latest request to guardian

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,4 +1,22 @@
 // receives traffic from the router.
+var request = require('request');
+
+var search = module.exports = {};
+
+var url = 'http://content.guardianapis.com/search?order-by=newest&api-key=test';
+
+search.latest = function(req, res) {
+  var url = "http://content.guardianapis.com/search?order-by=newest&api-key=test"
+
+  request(url, function (error, response, body) {
+    if (error) {
+      res.writeHead(500, {"content-type" : "text/html"});
+      res.end("<h1>Internal Server Error :-| </h1>");
+    }
+    console.log(body);
+  });
+
+}
 
 
 // handles requests from the user eventListener / (request.js)

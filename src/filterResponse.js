c74a08a60efb522dbd0417f35ea703ded7501ae0
Macintosh Helper <noreply@macintoshhelper.com>
2017-03-23 11:31:53 +0200

    Issue #43 - Replaced forEach with map in src/filterResponse

--- src/filterResponse.js
+++ src/filterResponse.js
@@ -11,14 +11,19 @@ module.exports = function(response) {
     var results = response.response.results;
     filteredResponse.results = [];
 
-    results.forEach(function(result, i){
-        filteredResponse.results[i] = {};
-        filteredResponse.results[i].id = result.id;
-        filteredResponse.results[i].sectionName = result.sectionName;
-        filteredResponse.results[i].webPublicationDate = result.webPublicationDate;
-        filteredResponse.results[i].title = result.webTitle;
-        filteredResponse.results[i].webUrl = result.webUrl;
 
+    filteredResponse.results = results.map((result) => {
+      var newResult = {};
+
+      newResult.id = result.id;
+      newResult.sectionName = result.sectionName;
+      newResult.webPublicationDate = result.webPublicationDate;
+      newResult.title = result.webTitle;
+      newResult.webUrl = result.webUrl;
+
+
+      return newResult;
     })
+
     return filteredResponse;
 }
